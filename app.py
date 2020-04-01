b68c8877211dba4a8bf9180f219061d217c08c7d
Alvin <qazqwe0922@gmail.com>
2020-04-01 13:44:16 +0800

    search page

--- app.py
+++ app.py
@@ -1,34 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pymysql
 app = Flask(__name__)
 conn = pymysql.connect(
-    host='localhost',
+    host='127.0.0.1',
     port=3306,
     user='root',
     passwd='0000',
     db='iosclub',
     charset='utf8mb4',
 )
-with conn.cursor() as cursor:
-    sql = 'SELECT * FROM memberList;'
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    for i in results:
-        print(i)
 
 
 @app.route('/')  # address
-def hello_world():
-    return render_template('index.html', name='alvin')
+def hello_world(name=None):
+    return render_template('index.html', name=name)
 
 
-@app.route('/<name>', methods=['GET', 'POST'])
-def detail(name=None):
-    with conn.cursor() as cursor:
-        sql = "SELECT * FROM memberList WHERE member_name =%s;"
-        cursor.execute(sql, name)
-        results = cursor.fetchall()
-        return str(results)
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'GET':
+        return render_template('search.html', results=None)
+    elif request.method == 'POST':
+        with conn.cursor() as cursor:
+            name = request.form['name']
+            sql = "SELECT * FROM memberList WHERE member_name =%s;"
+            cursor.execute(sql, name)
+            results = cursor.fetchall()[0]
+            return render_template('search.html', results=results)
 
 
 if __name__ == '__main__':
