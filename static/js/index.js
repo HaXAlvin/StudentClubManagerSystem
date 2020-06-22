31bd3b3cecdedf598e7a530a7a14fdc1c4af812f
amazingSuger <qwer23655441@gmail.com>
2020-06-22 15:40:35 +0800

    pic

--- static/js/index.js
+++ static/js/index.js
@@ -5,15 +5,19 @@ var navClass = $(".logo_nav");
 $(document).ready(function () {
   navClass.removeClass("hideUp");
   bodyClass.addClass("hideUp");
-  $.get("/account_check", function( data ) {
-    console.log(data);
-    if(!data.login) {
-      $(".member_dropdown").hide();
-    }
-    if(!data.login||!data.manager){
-      $(".manager_dropdown").hide();
-    }
-  }, "json" );
+  $.get(
+    "/account_check",
+    function (data) {
+      console.log(data);
+      if (!data.login) {
+        $(".member_dropdown").hide();
+      }
+      if (!data.login || !data.manager) {
+        $(".manager_dropdown").hide();
+      }
+    },
+    "json"
+  );
 });
 
 // var bodyClass = document.body.classList,
@@ -36,11 +40,11 @@ window.addEventListener("scroll", function () {
   }
   lastScrollY = st;
 });
-window.addEventListener("beforeunload", function (event) {
-  event.returnValue = "您即將離開此網頁";
-  //離開網頁之提示
-});
-window.onbeforeunload = function (event) {
-  event.returnValue = "您即將離開此網頁";
-  //離開網頁之提示
-};
+// window.addEventListener("beforeunload", function (event) {
+//   event.returnValue = "您即將離開此網頁";
+//   //離開網頁之提示
+// });
+// window.onbeforeunload = function (event) {
+//   event.returnValue = "您即將離開此網頁";
+//   //離開網頁之提示
+// };
